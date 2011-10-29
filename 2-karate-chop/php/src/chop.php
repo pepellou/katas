ffe101d6109c191135102651265c999ba0aa52a4
Pepe Doval <pepellou@gmail.com>
2011-10-30 01:30:39 +0200

    Refactor

--- 2-karate-chop/php/src/chop.php
+++ 2-karate-chop/php/src/chop.php
@@ -1,23 +1,33 @@
 <?php
 
-function chopArray(
-	$toSearch,
-	$values
-) {
-	$left = 0;
-	$right = count($values) - 1;
+abstract class Chop {
 
-	while ($left <= $right) {
-		$middle = round(($right + $left) / 2);
-		if ($values[$middle] == $toSearch)
-			return $middle;
-		if ($values[$middle] < $toSearch)
-			$left = $middle + 1;
-		else
-			$right = $middle - 1;
+	public abstract function chopArray($toSearch, $values);
+
+}
+
+class IterativeChop extends Chop {
+
+	public function chopArray(
+		$toSearch,
+		$values
+	) {
+		$left = 0;
+		$right = count($values) - 1;
+	
+		while ($left <= $right) {
+			$middle = round(($right + $left) / 2);
+			if ($values[$middle] == $toSearch)
+				return $middle;
+			if ($values[$middle] < $toSearch)
+				$left = $middle + 1;
+			else
+				$right = $middle - 1;
+		}
+	
+		return -1;
 	}
 
-	return -1;
 }
 
 ?>
--- 2-karate-chop/php/test/ChopTest.php
+++ 2-karate-chop/php/test/ChopTest.php
@@ -6,25 +6,30 @@ class ChopTest extends PHPUnit_Framework_TestCase {
 
 	public function test_all(
 	) {
-		$this->assertEquals(-1, chopArray(3, array()));
-		$this->assertEquals(-1, chopArray(3, array(1)));
-		$this->assertEquals(0,  chopArray(1, array(1)));
-		$this->assertEquals(0,  chopArray(1, array(1, 3, 5)));
-		$this->assertEquals(1,  chopArray(3, array(1, 3, 5)));
-		$this->assertEquals(2,  chopArray(5, array(1, 3, 5)));
-		$this->assertEquals(-1, chopArray(0, array(1, 3, 5)));
-		$this->assertEquals(-1, chopArray(2, array(1, 3, 5)));
-		$this->assertEquals(-1, chopArray(4, array(1, 3, 5)));
-		$this->assertEquals(-1, chopArray(6, array(1, 3, 5)));
-		$this->assertEquals(0,  chopArray(1, array(1, 3, 5, 7)));
-		$this->assertEquals(1,  chopArray(3, array(1, 3, 5, 7)));
-		$this->assertEquals(2,  chopArray(5, array(1, 3, 5, 7)));
-		$this->assertEquals(3,  chopArray(7, array(1, 3, 5, 7)));
-		$this->assertEquals(-1, chopArray(0, array(1, 3, 5, 7)));
-		$this->assertEquals(-1, chopArray(2, array(1, 3, 5, 7)));
-		$this->assertEquals(-1, chopArray(4, array(1, 3, 5, 7)));
-		$this->assertEquals(-1, chopArray(6, array(1, 3, 5, 7)));
-		$this->assertEquals(-1, chopArray(8, array(1, 3, 5, 7)));
+		$implementations = array(
+			new IterativeChop()
+		);
+		foreach ($implementations as $imp) {
+			$this->assertEquals(-1, $imp->chopArray(3, array()));
+			$this->assertEquals(-1, $imp->chopArray(3, array(1)));
+			$this->assertEquals(0,  $imp->chopArray(1, array(1)));
+			$this->assertEquals(0,  $imp->chopArray(1, array(1, 3, 5)));
+			$this->assertEquals(1,  $imp->chopArray(3, array(1, 3, 5)));
+			$this->assertEquals(2,  $imp->chopArray(5, array(1, 3, 5)));
+			$this->assertEquals(-1, $imp->chopArray(0, array(1, 3, 5)));
+			$this->assertEquals(-1, $imp->chopArray(2, array(1, 3, 5)));
+			$this->assertEquals(-1, $imp->chopArray(4, array(1, 3, 5)));
+			$this->assertEquals(-1, $imp->chopArray(6, array(1, 3, 5)));
+			$this->assertEquals(0,  $imp->chopArray(1, array(1, 3, 5, 7)));
+			$this->assertEquals(1,  $imp->chopArray(3, array(1, 3, 5, 7)));
+			$this->assertEquals(2,  $imp->chopArray(5, array(1, 3, 5, 7)));
+			$this->assertEquals(3,  $imp->chopArray(7, array(1, 3, 5, 7)));
+			$this->assertEquals(-1, $imp->chopArray(0, array(1, 3, 5, 7)));
+			$this->assertEquals(-1, $imp->chopArray(2, array(1, 3, 5, 7)));
+			$this->assertEquals(-1, $imp->chopArray(4, array(1, 3, 5, 7)));
+			$this->assertEquals(-1, $imp->chopArray(6, array(1, 3, 5, 7)));
+			$this->assertEquals(-1, $imp->chopArray(8, array(1, 3, 5, 7)));
+		}
 	}
 
 }
